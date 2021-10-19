627b20b57696ccdfae649873e4e4a14507a6e91a
leonardmoravec <leonardmoravec154@gmai.lcom>
2021-10-19 20:43:12 +0200

    I parsed string to int and checked if the value passed is lower than 10.

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -6,7 +6,17 @@ namespace Open_Lab_01._01
     {
         public bool IsLessThan10(string input)
         {
-            throw new NotImplementedException();
+            int output;
+            Int32.TryParse(input, out output);
+            if (output < 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
         }
     }
 }
